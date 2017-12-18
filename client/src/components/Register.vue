6e20fedf785e2f75d23980fb60d0e335c90743c3
Alban OP <alban.leandri@gmail.com>
2017-12-19 00:26:32 +0700

    Added connection to db and style with Vuetify for Register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,44 +1,62 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+          type="email"
+          name="email"
+          v-model="email"
+          placeholder="Enter your email" />
+          <br>
+          <input
+          type="password"
+          name="password"
+          v-model="password"
+          placeholder="Enter your password" />
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+          </div>
+      </v-flex>
+    </v-layout>
+  </template>
 
-    <input
-    type="email"
-    name="email"
-    v-model="email"
-    placeholder="Enter your email"/>
-    <br>
-    <input
-    type="password"
-    name="password"
-    v-model="password"
-    placeholder="Enter your password"/>
-    <br>
-    <button @click="register">Register</button>
+  <script>
+  import AuthenticationService from '@/services/AuthenticationService'
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        error: null
+      }
+    },
+    methods: {
 
-  </div>
-</template>
-
-<script>
-import AuthenticationService from '@/services/AuthenticationService'
-export default {
-  data () {
-    return {
-      email: '',
-      password: ''
-    }
-  },
-  methods: {
-    async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      async register () {
+        try {
+          const response = await AuthenticationService.register({
+            email: this.email,
+            password: this.password
+          })
+          console.log(response.data)
+        } catch (error) {
+          this.error = error.response.data.error
+        }
+      }
     }
   }
-}
 </script>
 
 <style scoped>
+
+.error {
+  color: red;
+}
 </style>
