50ee0a9a6b081999d2eda82171e99983003335b3
Alban OP <alban.leandri@gmail.com>
2017-12-24 19:25:40 +0700

    added token JWT and bcrypt password hashing

--- server/src/app.js
+++ server/src/app.js
@@ -12,8 +12,8 @@ app.use(cors())
 
 require('./routes')(app)
 
-sequelize.sync()
+sequelize.sync({force: false})
   .then(() => {
     app.listen(config.port)
-    console.log('Server started on port ${config.port}')
+    console.log(`Server started on port ${config.port}`);
   })
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -9,19 +9,21 @@ function jwtSignUser (user) {
   })
 }
 
-
 module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
-      } catch (err) {
-        res.status(400).send({
-          error: 'Sorry, this email has already been registered.'
-        })
-      }
-    },
-
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
+    }
+  },
   async login (req, res) {
     try {
       const {email, password} = req.body
@@ -31,17 +33,16 @@ module.exports = {
         }
       })
 
-      console.log('user', user.toJSON())
       if (!user) {
         return res.status(403).send({
-          error: 'This email is not registered'
+          error: 'The login information was incorrect'
         })
       }
 
-      const isPasswordValid = password === user.password
+      const isPasswordValid = await user.comparePassword(password)
       if (!isPasswordValid) {
         return res.status(403).send({
-          error: 'The password is wrong'
+          error: 'The login information was incorrect'
         })
       }
 
@@ -51,9 +52,9 @@ module.exports = {
         token: jwtSignUser(userJson)
       })
     } catch (err) {
-        res.status(500).send({
-          error: 'Sorry, an error occured during the login...'
-        })
-      }
+      res.status(500).send({
+        error: 'An error has occured trying to log in'
+      })
     }
   }
+}
