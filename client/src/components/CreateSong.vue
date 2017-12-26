cbcd6db06856c0c5b89c30a0c3b0cd408fb9a8a0
Alban OP <alban.leandri@gmail.com>
2017-12-26 17:36:52 +0700

    Beautified the Songs page and created redirection to view song page

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -2,27 +2,32 @@
   <v-layout>
     <v-flex xs4>
       <panel title="Song Metadata">
-        <v-text-field label="Title" v-model="song.title">
+        <v-text-field label="Title" required :rules="[required]" v-model="song.title">
         </v-text-field>
-        <v-text-field label="Artist" v-model="song.artist">
+        <v-text-field label="Artist" required :rules="[required]" v-model="song.artist">
         </v-text-field>
-        <v-text-field label="Genre" v-model="song.genre">
+        <v-text-field label="Genre" required :rules="[required]" v-model="song.genre">
         </v-text-field>
-        <v-text-field label="Album" v-model="song.album">
+        <v-text-field label="Album" required :rules="[required]" v-model="song.album">
         </v-text-field>
-        <v-text-field label="Album Image URL" v-model="song.albumImageUrl">
+        <v-text-field label="Album Image URL" required :rules="[required]" v-model="song.albumImageUrl">
         </v-text-field>
-        <v-text-field label="Youtube ID" v-model="song.youtubeId">
+        <v-text-field label="Youtube ID" required :rules="[required]" v-model="song.youtubeId">
         </v-text-field>
       </panel>
     </v-flex>
     <v-flex xs8>
       <panel title="Song Structure" class="ml-2">
-        <v-text-field label="Lyrics" v-model="song.lyrics" multi-line>
+        <v-text-field label="Lyrics" required :rules="[required]" v-model="song.lyrics" multi-line>
         </v-text-field>
-        <v-text-field label="Tab" v-model="song.tab" multi-line>
+        <v-text-field label="Tab" required :rules="[required]" v-model="song.tab" multi-line>
         </v-text-field>
       </panel>
+
+      <v-alert  class="ml-4" :value="error" transition="scale-transition" error>
+        {{error}}
+      </v-alert>
+
       <v-btn dark class="cyan" @click="create">
         Create this song !
       </v-btn>
@@ -45,11 +50,21 @@
           youtubeId: null,
           lyrics: null,
           tab: null
-        }
+        },
+        error: null,
+        required: (value) => !!value || 'This field is required.'
       }
     },
     methods: {
       async create () {
+        this.error = null
+        const areAllFieldsCompleted = Object
+          .keys(this.song)
+          .every(key => !!this.song[key])
+        if (!areAllFieldsCompleted) {
+          this.error = 'Please fill in all the required fields'
+          return
+        }
         try {
           await SongsService.post(this.song)
           this.$router.push({
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,10 +2,39 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-        <div v-for="song in songs" :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+        <v-btn slot="action" :to="{ name: 'songs-create' }" class="blue" fab light medium absolute right middle>
+          <v-icon>
+            add
+          </v-icon>
+        </v-btn>
+
+        <div class="song" v-for="song in songs" :key="song.id">
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-gemre">
+                {{song.genre}}
+              </div>
+
+              <v-btn dark class="cyan" @click="navigateTo({
+                name: 'song',
+                params: {
+                  songId: song.id
+                }
+              })">
+                View
+              </v-btn>
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageUrl" />
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -13,23 +42,49 @@
 </template>
 
 <script>
-import SongsService from '@/services/SongsService'
-import Panel from '@/components/Panel'
-export default {
-  components: {
-    Panel
-  },
-  data () {
-    return {
-      songs: null
+  import SongsService from '@/services/SongsService'
+  import Panel from '@/components/Panel'
+  export default {
+    components: {
+      Panel
+    },
+    data () {
+      return {
+        songs: null
+      }
+    },
+    methods: {
+      navigateTo (route) {
+        this.$router.push(route)
+      }
+    },
+    async mounted () {
+      this.songs = (await SongsService.index()).data
     }
-  },
-  async mounted () {
-    this.songs = (await SongsService.index()).data
   }
-}
 </script>
 
 <style scoped>
+  .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
 
+  .song-title {
+    font-size: 30px;
+  }
+
+  .song-artist {
+    font-size: 24px;
+  }
+
+  .song-genre {
+    font-size: 18px;
+  }
+
+  .album-image {
+    width: 70%;
+    margin: 0 auto;
+  }
 </style>
