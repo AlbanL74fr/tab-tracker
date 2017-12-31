084904337bdefa59375f76ef5aaef39fed522226
Alban OP <alban.leandri@gmail.com>
2017-12-31 14:30:23 +0700

    Added boomark kist in song page

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -53,7 +53,8 @@
     },
     computed: {
       ...mapState([
-        'isUserLoggedIn'
+        'isUserLoggedIn',
+        'user'
       ])
     },
     watch: {
@@ -61,11 +62,15 @@
         if (!this.isUserLoggedIn) {
           return
         }
+
         try {
-          this.bookmark = (await BookmarksService.index({
+          const bookmarks = (await BookmarksService.index({
             songId: this.song.id,
-            userId: this.$store.state.user.id
+            userId: this.user.id
           })).data
+          if (bookmarks.length) {
+            this.bookmark = bookmarks[0]
+          }
         } catch (err) {
           console.log(err)
         }
@@ -76,7 +81,7 @@
         try {
           this.bookmark = (await BookmarksService.post({
             songId: this.$store.state.route.params.songId,
-            userId: this.$store.state.user.id
+            userId: this.user.id
           })).data
         } catch (err) {
           console.log(err)
--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -1,62 +1,76 @@
-const { Bookmark } = require('../models')
+const { Bookmark, Song, User } = require("../models");
+const _ = require("lodash");
 
 module.exports = {
   async index (req, res) {
     try {
-      const { songId, userId } = req.query
-
-      const bookmark = await Bookmark.findOne({
-        where: {
-          SongId: songId,
-          UserId: userId
-        }
+      const { songId, userId } = req.query;
+      const where = {
+        UserId: userId
+      }
+      if (songId) {
+        where: SongId = songId;
+      }
+      const bookmarks = await Bookmark.findAll({
+        where: where,
+        include: [
+          {
+            model: Song
+          }
+        ]
       })
-      res.send(bookmark)
+        .map(bookmark => bookmark.toJSON())
+        .map(bookmark => _.extend(
+            {},
+            bookmark.Song,
+            bookmark
+          ))
+      res.send(bookmarks);
     } catch (err) {
       res.status(500).send({
-        error: 'An error has occured while trying to fetch the bookmarks'
-      })
+        error: "An error has occured while trying to fetch the bookmarks"
+      });
     }
   },
 
   async post (req, res) {
     try {
-      const {songId, userId} = req.body
+      const { songId, userId } = req.body;
       const bookmark = await Bookmark.findOne({
         where: {
           SongId: songId,
           UserId: userId
         }
-      })
+      });
       if (bookmark) {
         return res.status(400).send({
-          error: 'you already have this set as a bookmark'
-        })
+          error: "you already have this set as a bookmark"
+        });
       }
       const newBookmark = await Bookmark.create({
         SongId: songId,
         UserId: userId
-      })
-        
-      res.send(newBookmark)
+      });
+
+      res.send(newBookmark);
     } catch (err) {
-      console.log(err)
+      console.log(err);
       res.status(500).send({
-        error: 'an error has occured trying to create the bookmark'
-      })
+        error: "an error has occured trying to create the bookmark"
+      });
     }
   },
 
   async delete (req, res) {
     try {
-      const {bookmarkId} = req.params
-      const bookmark = await Bookmark.findById(bookmarkId)
-      await bookmark.destroy()
-      res.send(bookmark)
+      const { bookmarkId } = req.params;
+      const bookmark = await Bookmark.findById(bookmarkId);
+      await bookmark.destroy();
+      res.send(bookmark);
     } catch (err) {
       res.status(500).send({
         error: `An error has occured trying to delete the bookmark`
-      })
+      });
     }
   }
-}
+};
